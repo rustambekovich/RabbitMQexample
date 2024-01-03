656728fef8724b765c0a7731371bc0f20d5cf910
rustambekovich <muhammadqodir5050@gmail.com>
2024-01-03 17:18:11 +0500

    add terminal sender and resived project

---
+++ src/ReseiverTerminal/ReseiverRabbitMQ/Program.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = "localhost",
+        };
+
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "first",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var response = new EventingBasicConsumer(channel);
+
+            response.Received += (model, convertToString) =>
+            {
+                var body = convertToString.Body.ToArray();
+                var massage = Encoding.UTF8.GetString(body);
+
+                Console.WriteLine("RabbitMQ massage: " + massage);
+            };
+
+            channel.BasicConsume(queue: "first",
+                                 autoAck: true,
+                                 consumer: response);
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ src/SenderTerminal/SenderRabbitMQ/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = "localhost",
+        };
+
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "first",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            Console.Write("Enter massage: ");
+            string message = Console.ReadLine()!;
+
+            var request = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "",
+                                 routingKey: "first",
+                                 basicProperties: null,
+                                 body: request);
+
+            Console.WriteLine("Massage send rabbitMQ");
+        }
+
+        Console.WriteLine("1. Exit application.\n" +
+                          "2.Back to the beginning.");
+        Console.Write("Number: ");
+        int step = int.Parse(Console.ReadLine()!);
+
+        if (step == 2)
+            Main(args);
+    }
+}
